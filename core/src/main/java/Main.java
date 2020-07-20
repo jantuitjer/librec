b8489c0f8011efd2fb38959ad4a6bb3548ff960b
jan tuitjer <jan.tuitjer@stud.hs-hannover.de>
2020-07-20 12:35:50 +0200

    added starter class of HybridRecommenderJob

---
+++ core/src/main/java/Main.java
@@ -0,0 +1,116 @@
+import net.librec.common.LibrecException;
+import net.librec.conf.Configuration;
+import net.librec.conf.HybridConfiguration;
+import net.librec.data.DataModel;
+import net.librec.data.model.TextDataModel;
+import net.librec.eval.RecommenderEvaluator;
+import net.librec.eval.rating.MAEEvaluator;
+import net.librec.job.HybridRecommenderJob;
+import net.librec.recommender.AbstractRecommender;
+import net.librec.recommender.HybridContext;
+import net.librec.recommender.RecommenderContext;
+import net.librec.recommender.cf.ItemKNNRecommender;
+import net.librec.recommender.cf.UserKNNRecommender;
+import net.librec.recommender.hybrid.WeightedHybridRecommender;
+import net.librec.similarity.CosineSimilarity;
+import net.librec.similarity.PCCSimilarity;
+import net.librec.similarity.RecommenderSimilarity;
+import org.apache.commons.cli.MissingArgumentException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author Jan Tuitjer
+ */
+public class Main {
+    static final String FILE_PATH_USER = "conf/hybridConfigs/userknn-test.properties";
+    static final String FILE_PATH_ITEM = "conf/hybridConfigs/itemknn-test.properties";
+    private static HybridConfiguration HybridConfiguration;
+
+    public static void main(String[] args) throws LibrecException, IOException, ClassNotFoundException, MissingArgumentException {
+//        final int RUNS = 50;
+//        double mae_sum=0.0;
+//        for (int i = 0; i < RUNS; i++) {
+//            mae_sum+= hybridRun();
+//        }
+//        System.out.println("mae_sum/RUNS = " + mae_sum/RUNS);
+        hybridJobExecution("conf/hybridconfig.properties");
+    }
+
+    private static double hybridRun() throws FileNotFoundException, LibrecException {
+
+        Configuration confUser = new Configuration();
+        Configuration confItem = new Configuration();
+        config.ConfigurationParser.parse(FILE_PATH_USER, confUser);
+        config.ConfigurationParser.parse(FILE_PATH_ITEM, confItem);
+        ArrayList<Configuration> confs = new ArrayList<Configuration>() {
+            {
+                add(confUser);
+                add(confItem);
+            }
+        };
+        DataModel userData = new TextDataModel(confUser);
+        DataModel itemData = new TextDataModel(confItem);
+        userData.buildDataModel();
+        itemData.buildDataModel();
+        ArrayList<DataModel> models = new ArrayList<DataModel>() {
+            {
+                add(userData);
+                add(itemData);
+            }
+        };
+        RecommenderSimilarity userSim = new CosineSimilarity();
+        RecommenderSimilarity itemSim = new PCCSimilarity();
+        userSim.buildSimilarityMatrix(userData);
+        itemSim.buildSimilarityMatrix(itemData);
+        ArrayList<RecommenderSimilarity> sims = new ArrayList<RecommenderSimilarity>() {
+            {
+                add(userSim);
+                add(itemSim);
+            }
+        };
+        AbstractRecommender userRec = new UserKNNRecommender();
+        AbstractRecommender itemRec = new ItemKNNRecommender();
+        ArrayList<AbstractRecommender> recommdenders = new ArrayList<AbstractRecommender>() {
+            {
+                add(userRec);
+                add(itemRec);
+            }
+        };
+        HybridContext hybridContext = new HybridContext(confs,models,sims);
+        WeightedHybridRecommender whr = new WeightedHybridRecommender(recommdenders, hybridContext);
+        whr.trainModel();
+        RecommenderEvaluator eval = new MAEEvaluator();
+        double mae =  whr.evaluate(eval);
+        System.out.println("mae = " + mae);
+        return mae;
+    }
+
+    private static void test() throws FileNotFoundException, LibrecException {
+        Configuration conf = new Configuration();
+        config.ConfigurationParser.parse(FILE_PATH_ITEM, conf);
+        TextDataModel data = new TextDataModel(conf);
+        data.buildDataModel();
+        System.out.println("data = " + data.getDataConvertor().getPreferenceMatrix().rowSize());
+        System.out.println("data = " + data.getDataConvertor().getPreferenceMatrix().columnSize());
+        RecommenderSimilarity sim = new PCCSimilarity();
+        sim.buildSimilarityMatrix(data);
+        System.out.println("sim.getSimilarityMatrix().toSparseMatrix().rowSize() = " + sim.getSimilarityMatrix().getDim());
+        System.out.println("sim.getSimilarityMatrix().toSparseMatrix().columnSize() = " + sim.getSimilarityMatrix().row(sim.getSimilarityMatrix().getDim()).size());
+        RecommenderContext context = new RecommenderContext(conf, data, sim);
+        AbstractRecommender rec = new ItemKNNRecommender();
+        rec.train(context);
+        rec.recommendRank();
+        rec.getRecommendedList();
+    }
+
+    private static void hybridJobExecution(String _pathToHybridConfiguration) throws IOException, MissingArgumentException, LibrecException, ClassNotFoundException {
+        net.librec.conf.HybridConfiguration hybridConf = new HybridConfiguration(_pathToHybridConfiguration);
+        HybridRecommenderJob hrj = new HybridRecommenderJob(hybridConf);
+        hrj.runJob();
+    }
+}
