9dafa35c79539e2c78b9b3785fae05c5887085dc
jan tuitjer <jan.tuitjer@stud.hs-hannover.de>
2020-07-29 15:27:49 +0200

    cleaning

--- core/src/main/java/net/librec/recommender/hybrid/AbstractHybridRecommender.java
+++ core/src/main/java/net/librec/recommender/hybrid/AbstractHybridRecommender.java
@@ -38,12 +38,11 @@ public abstract class AbstractHybridRecommender extends AbstractRecommender {
         for (int i = 0; i < preferenceMatrix.rowSize(); i++) {
             recommendedItems.addList(new ArrayList<>());
         }
-//        RecommendedList recommendedItems = recommendedItemList;
         fillCommonTestSetAndMatrix(recommendedItems , commonTestMatrix);
         commonTestMatrix.reshape();
         recommendedItems.sort();
         for (AbstractRecommender rec : recommenders){
-            rec.getRecommendedList(recommendedItems );
+            rec.getRecommendedList(recommendedItems);
         }
         recommendedItemList = recommendedItems;
         return commonTestMatrix;
@@ -65,8 +64,6 @@ public abstract class AbstractHybridRecommender extends AbstractRecommender {
             int item = recommenders.get(0).getDataModel().getItemMappingData().get(commonItem.getItemId());
             double realValue = recommenders.get(0).getDataModel().getDataSplitter().getTestData().get(user, item);
             double predictedValue = commonItem.getValue();
-//            user = Integer.parseInt(commonItem.getUserId())-1;
-//            item = Integer.parseInt(commonItem.getItemId())-1;
             // add test entry into common test matrix
             _commonTestMatrix.set(user, item, realValue);
             //save previous/first user id
@@ -85,31 +82,12 @@ public abstract class AbstractHybridRecommender extends AbstractRecommender {
                 entryMap.put(user, itemValueList);
                 itemValueList.add(new KeyValue<>(item, predictedValue));
             }
-
-            // add common item into common recommendedItemList
-//            _recommendedList.add(user, item, predictedValue);
         }
-//        System.out.println("commonElements = " + commonElements.size());
-        int numElems = 0;
-//        _recommendedList = new RecommendedList(entryMap.keySet().size());
-//        for (int i = 0; i < entryMap.keySet().size(); i++) {
-//            _recommendedList.addList(new ArrayList<>());
-//        }
         for (Integer user : entryMap.keySet()){
-            numElems+= entryMap.get(user).size();
-//            _recommendedList.addList(new ArrayList<>());
             _recommendedList.setList(user, entryMap.get(user));
         }
-//        System.out.println("entryMap = " +numElems);
-//        System.out.println(_recommendedList.size());
-//        _recommendedList.reshape();
-//        System.exit(-564684);
-//        System.out.println("_commonTestMatrix1 = " + _commonTestMatrix.size());
         _commonTestMatrix.reshape();
         commonTestDataSet = _commonTestMatrix;
-//        System.out.println("_commonTestMatrix2 = " + _commonTestMatrix.size());
-//        System.out.println("AbstractHybridRecommender.fillCommonTestSetAndMatrix");
-//        System.out.println("_recommendedList = " + _recommendedList.size());
     }
 
     protected abstract ArrayList<RecommendedItem> handleRecommendedItems(AbstractRecommender recommender, ArrayList<RecommendedItem> commonElements) throws LibrecException;
@@ -140,11 +118,12 @@ public abstract class AbstractHybridRecommender extends AbstractRecommender {
         commonTestMatrix.reshape();
         recommendedItemList.sort();
         commonTestDataSet = commonTestMatrix;
-        //todo: remove prints
-        System.out.println("AbstractHybridRecommender.recommendRating");
-        System.out.println("commonTestDataSet = " + commonTestDataSet.size());
-        for (AbstractRecommender rec : recommenders){
-            System.out.println(rec.getClass().getSimpleName() + ": Test Data size: " +rec.getDataModel().getTestDataSet().size());
+        { //todo: remove prints
+            System.out.println("AbstractHybridRecommender.recommendRating");
+            System.out.println("commonTestDataSet = " + commonTestDataSet.size());
+            for (AbstractRecommender rec : recommenders) {
+                System.out.println(rec.getClass().getSimpleName() + ": Test Data size: " + rec.getDataModel().getTestDataSet().size());
+            }
         }
         return recommendedItemList;
     }
@@ -163,34 +142,6 @@ public abstract class AbstractHybridRecommender extends AbstractRecommender {
     @Override
     public RecommendedList recommendRank(LibrecDataList<AbstractBaseDataEntry> dataList) throws LibrecException {
         throw new NotImplementedException();
-    }
-
-    public double evaluate(RecommenderEvaluator _eval) throws LibrecException {
-        double result = 0.0d;
-        //TODO: change for other data set than filmtrust
-//        RecommendedList recommendedItems = new RecommendedItemList(1508);
-//        SequentialAccessSparseMatrix preferenceMatrix = (((AbstractDataModel) recommenders.get(0).getDataModel()).getDataConvertor().getPreferenceMatrix());
-        SequentialAccessSparseMatrix commonTestMatrix = initCommonTestSetAndMatrix();
-//        SequentialAccessSparseMatrix commonTestMatrix = initEmptyTestMatrixEntries(preferenceMatrix);
-        for (AbstractRecommender rec: recommenders){
-            rec.recommendRating(rec.getDataModel().getTestDataSet());
-        }
-//        getTestSetPredictions(recommendedItems, commonTestMatrix);
-//        fillCommonTestSetAndMatrix(recommendedItems, commonTestMatrix);
-//        System.out.println("commonTestMatrix.size() = " + commonTestMatrix.size());
-//        System.out.println("recommendedItems = " + recommendedItemList.size());
-        commonTestMatrix.reshape();
-        recommendedItemList.sort();
-//        System.out.println("commonTestMatrix.size() = " + commonTestMatrix.size());
-//        System.out.println("recommendedItems = " + recommendedItemList.size());
-       // if(_eval instanceof MAEEvaluator)
-        //    result = ((MAEEvaluator) _eval).evaluate(commonTestMatrix, recommendedItemList);
-//        else if (_eval instanceof RMSEEvaluator)
-//            result = ((RMSEEvaluator) _eval).evaluate(commonTestMatrix, recommendedItems);
-//        else if (_eval instanceof PrecisionEvaluator)
-//            result = ((PrecisionEvaluator) _eval).evaluate(commonTestMatrix, recommendedItems);
-        return result;
-
     }
 
     public RecommendedList recommendRating() throws LibrecException {
