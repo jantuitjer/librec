7032b65176216f664e55f9d1e8cd76ee46ac1c72
jan tuitjer <jan.tuitjer@stud.hs-hannover.de>
2020-08-07 15:28:47 +0200

    fix data path in configs

--- semanticweb/sparql_to_arff.py
+++ semanticweb/sparql_to_arff.py
@@ -161,7 +161,7 @@ def insert_pos_info():
 
 
 def get_arff_header():
-	header = '@Relation user_movie_rating'
+	header = '@Relation user_movie_rating\n'
 	header += '@Attribute user string\n'
 	header += '@Attribute item string\n'
 	header += '@Attribute rating NUMERIC\n'
