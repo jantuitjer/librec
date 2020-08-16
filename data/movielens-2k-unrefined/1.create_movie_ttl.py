a93c8fb22e5f5501eba636bc125d243c5e8099b1
Jan Tuitjer <jan.tuitjer@stud.hs-hannover.de>
2020-08-16 13:04:34 +0200

    added script for movie_region ttl added script for ttl functionality

---
+++ data/movielens-2k-unrefined/1.create_movie_ttl.py
@@ -0,0 +1,184 @@
+import os
+import sys
+from datetime import datetime
+from ttl_util import make_ttl_string, get_header 
+os.chdir(os.path.dirname(sys.argv[0]))
+
+
+region_dict = {
+'Afghanistan': 'CentralAsia',
+'Kazakhstan' : 'CentralAsia',
+'China' : 'EastAsia',
+'HongKong': 'EastAsia',
+'Japan' : 'EastAsia',
+'SoutKorea' : 'EastAsia',
+'Taiwan': 'EastAsia', 
+'Bhutan': 'SouthAsia',
+'India': 'SouthAsia',
+'Philippines' : 'SouthEastAsia',
+'Singapore' : 'SouthEastAsia',
+'Thailand': 'SouthEastAsia',
+'Vietnam': 'SouthEastAsia',
+'OccupiedPalestinianTerritory': 'WesternAsia',
+'Iran': 'WesternAsia',
+'Israel': 'WesternAsia',
+'Turkey': 'WesternAsia',
+'Algeria' : 'Africa',
+'BurkinaFaso' : 'Africa',
+'Botswana' : 'Africa',
+'IvoryCoast': 'Africa',
+'Libya': 'Africa',
+'Senegal': 'Africa',
+'SouthAfrica': 'Africa',
+'Tunisia': 'Africa',
+'Denmark': 'NorthEurope',
+'Finland': 'NorthEurope',
+'Iceland': 'NorthEurope',
+'Norway': 'NorthEurope',
+'Sweden': 'NorthEurope',
+'Austria': 'CentralEurope',
+'Croatia': 'CentralEurope',
+'CzechRepublic': 'CentralEurope',
+'Czechoslovakia': 'CentralEurope',
+'EastGermany': 'CentralEurope',
+'FederalRepublicofYugoslavia': 'CentralEurope',
+'Germany': 'CentralEurope',
+'Hungary': 'CentralEurope',
+'Poland': 'CentralEurope',
+'Switzerland': 'CentralEurope',
+'WestGermany': 'CentralEurope',
+'Yugoslavia': 'CentralEurope',
+'Russia': 'EasternEurope',
+'SovietUnion': 'EasternEurope',
+'Bulgaria': 'SouthEasternEurope',
+'Romania': 'SouthEasternEurope',
+'BosniaandHerzegovina': 'SouthEasternEurope',
+'RepublicofMacedonia': 'SouthEasternEurope',
+'Greece': 'SouthEasternEurope',
+'Belgium': 'WesternEurope',
+'France': 'WesternEurope',
+'Ireland': 'WesternEurope',
+'Netherlands': 'WesternEurope',
+'UK': 'WesternEurope',
+'Italy': 'SouthernEurope',
+'Portugal': 'SouthernEurope',
+'Spain': 'SouthernEurope',
+'Aruba': 'Caribbean',
+'Cuba': 'Caribbean',
+'Jamaica': 'Caribbean',
+'Canada': 'NorthAmerica',
+'Mexico': 'NorthAmerica',
+'USA': 'NorthAmerica',
+'Argentina': 'SouthAmerica',
+'Brazil': 'SouthAmerica',
+'Chile': 'SouthAmerica',
+'Colombia': 'SouthAmerica',
+'Peru': 'SouthAmerica',
+'Venezuela': 'SouthAmerica',
+'Australia': 'Oceania',
+'NewZealand': 'Oceania',
+'\t': 'Unknown',
+'\n': 'Unknown',
+None: 'Unknown',
+'': 'Unknown'}
+
+movie_info_dict = dict()
+country_dict = dict()
+def read_movie_file():
+    f = open('movies.dat')
+    next(f)
+    pos_id = 0
+    pos_year = 5
+    for line in f:
+        entries = line.split('\t')
+        movie_id = entries[pos_id]
+        year = entries[pos_year]
+        movie_info_dict[movie_id] ={'year': year}
+    f.close()
+    
+def read_genre_file():
+    f = open('movie_genres.dat')
+    next(f)
+    for line in f:
+        movie_id, genre = line.split('\t')
+        genre = genre.strip()
+        if movie_info_dict.get(movie_id).get('genres') is None:
+            movie_info_dict.get(movie_id)['genres'] = [genre]
+        else:
+            movie_info_dict.get(movie_id).get('genres').append(genre)
+    f.close()
+    
+def read_country_file():
+    f = open('movie_countries.dat')
+    next(f)
+    for line in f:
+        movie_id, country = line.split('\t')
+        if country == '\n':
+            country = 'Unknown'
+        country = country.strip().replace(' ','')
+        movie_info_dict.get(movie_id)['country'] = country
+        if country_dict.get(country) is None:
+            country_dict[country] = country
+    f.close()
+def read_director_file():
+    f = open('movie_directors.dat')
+    next(f)
+    for line in f:
+        movie_id, director, director_name = line.split('\t')
+        director = director.strip()
+        movie_info_dict.get(movie_id)['director'] = director
+
+def read_ratings_file():
+    f = open('user_ratedmovies-timestamps.dat')
+    next(f)
+    for line in f:
+        user_id, movie_id, rating, timestamp = line.split('\t')
+        timestamp = timestamp.strip()
+        if movie_info_dict.get(movie_id).get('sum_ratings') is None:
+            movie_info_dict.get(movie_id).update({'sum_ratings' : float(rating), 'amt_ratings' : 1})
+        else:
+            new_rating_val = movie_info_dict.get(movie_id).get('sum_ratings') + float(rating)
+            new_rating_amt = movie_info_dict.get(movie_id).get('amt_ratings') + 1
+            movie_info_dict.get(movie_id).update({'sum_ratings' : new_rating_val, 'amt_ratings' : new_rating_amt})
+    for movie_id in movie_info_dict:
+        amt_ratings = movie_info_dict.get(movie_id).get('amt_ratings')
+        val_ratings = movie_info_dict.get(movie_id).get('sum_ratings')
+        if val_ratings is None and amt_ratings is None:
+            pass
+        else:
+            avg_rating = val_ratings / amt_ratings
+            movie_info_dict.get(movie_id)['avg_rating'] = avg_rating
+    f.close()
+    
+def write_movie_region_ttl():
+    f = open('../../semanticweb/abox/movie_info.ttl', 'w')
+    f.write(get_header())
+    for movie_id in movie_info_dict:
+        movie = movie_info_dict.get(movie_id)
+        list = ['movie:{}'.format(movie_id), 'a', 'dbo:Film']
+        list.extend(['jt:yearOfFilming', movie.get('year')])
+        if movie.get('amt_ratings') is not None:
+            list.extend(['jt:numberOfRatings', movie.get('amt_ratings')])
+            list.extend(['jt:avgRating', movie.get('avg_rating')])
+        list.extend(['jt:shotIn', 'country:{}'.format(movie.get('country'))])
+        for genre in movie.get('genres'):
+            list.extend(['jt:ofGenre', 'genre:{}'.format(genre)])
+        f.write(make_ttl_string(list))
+    for country in country_dict:
+        f.write(make_ttl_string(['country:{}'.format(country),'jt:partOf', 
+            'region:{}'.format(region_dict.get(country))]))
+    f.close()
+
+def get_data():
+    read_movie_file()
+    read_genre_file()
+    read_country_file()
+    read_director_file()
+    read_ratings_file()
+
+def main():
+    get_data()
+    write_movie_region_ttl()
+
+if __name__ == "__main__":
+    main()
---
+++ data/movielens-2k-unrefined/ttl_util.py
@@ -0,0 +1,41 @@
+def make_bracket_ttl(mvId: int, rating: float, time: str):
+	entry_line = 'jt:hasRated ex:m_{};\n\t jt:withRating {};\n\t jt:atTime {}'.format(mvId, rating, time)
+	entry = '\tjt:ratedMovie [\n\t{}];\n'.format(entry_line)
+	return entry
+    
+def make_ttl_string(_statements):
+	ttl = ''
+	if len(_statements) < 3:
+		print('to few elements to form statement')
+		exit(-9)
+	for i in range(1, len(_statements), 2):
+		if i == 1:
+			ttl = '{}\t{}\t{}'.format(_statements[i-1], _statements[i], _statements[i+1])
+			continue
+		ttl = '{};\n\t{}\t{}'.format(ttl, _statements[i], _statements[i+1])
+	ttl += '.\n\n'
+	return ttl
+
+def get_header():
+    str = '@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n'
+    str += '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n'
+    str += '@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n'
+    str += '@prefix dbo: <http://dbpedia.org/ontology/> .\n'
+    str += '@prefix dbc: <http://dbpedia.org/resource/Category:> . \n'
+    str += '@prefix owl: <http://www.w3.org/2002/07/owl#> .\n'
+    str += '@prefix dcterms: <http://dublincore.org/documents/2012/06/14/dcmi-terms/> .\n'
+    str += '@prefix schema: <https://schema.org/place> .\n'
+    str += '@prefix jt: <http://schema.example.org/> .\n'
+    str += '@prefix xsd: <https://www.w3.org/2011/rdf-wg/wiki/XSD_Datatypes> .\n'
+    str += '@prefix movie:   <http://movie.example.org/>.\n'
+    str += '@prefix user:   <http://user.example.org/>.\n'
+    str += '@prefix director:   <http://director.example.org/>.\n'
+    str += '@prefix country:   <http://country.example.org/>.\n'
+    str += '@prefix genre:   <http://genre.example.org/>.\n'
+    str += '@prefix region:   <http://region.example.org/>.\n'
+    str += '@prefix fr:   <http://filmrating.example.org/>.\n'
+    str += '@prefix dr:   <http://directorrating.example.org/>.\n'
+    str += '@prefix rr:   <http://regionrating.example.org/>.\n'
+    str += '@prefix gr:   <http://genrerating.example.org/>.\n'
+    str += '@prefix ex:   <http://example.org/>.\n\n'
+    return str
